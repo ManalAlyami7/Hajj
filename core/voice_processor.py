1c5741041016d6f0c39c893bbaeaa4d6ef8b9aa9
Manalalyami7 <manalalyami7@gmail.com>
2025-11-06 14:18:31 +0300

    f

--- core/voice_processor.py
+++ core/voice_processor.py
@@ -66,6 +66,10 @@ class VoiceProcessor:
             # Auto-detect Arabic if language is missing
             if not language:
                 language = "arabic" if any("\u0600" <= ch <= "\u06FF" for ch in text) else "english"
+            #Find all of the words that sound like Hajj, but transcribed as hatch or similar and convert them to Hajj
+            words_like_hajj = ['hatch','hatching']
+            for word in words_like_hajj:
+                text = text.replace(word, 'Hajj')
 
             result = {
                 "text": text.strip(),
