fe7c6f9f3826a92760f50a67d940c927151b3a5f
Manalalyami7 <manalalyami7@gmail.com>
2025-11-07 15:45:15 +0300

    g

--- pages/voicebot.py
+++ pages/voicebot.py
@@ -420,7 +420,7 @@ st.markdown(f"""
 .status-indicator{{
   position: fixed;
   top: 15px;
-  {'left' if is_arabic else 'right'}: 300px;
+  {'left' if is_arabic else 'right'}: 340px;
   padding: 0.6rem 1.25rem;
   background: rgba(0, 0, 0, 0.75);
   border-radius: 2rem;
